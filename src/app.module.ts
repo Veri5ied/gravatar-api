d927394ab357394f143b3d29203516d755ad9c9e
Veri5ied <alvinuchenna14@gmail.com>
2024-07-29 16:09:17 +0100

    WIP: config module setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
